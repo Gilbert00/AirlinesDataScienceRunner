88d290e72e6960acea0631a0955dd9bf8322455e
gilbert00 <fkemper@mail.ru>
2020-08-15 14:25:30 +0300

    Adding BaseColumn

--- src/airlinesdatasciencerunner/AirlinesDataScienceRunner.java
+++ src/airlinesdatasciencerunner/AirlinesDataScienceRunner.java
@@ -5,6 +5,43 @@
  */
 package airlinesdatasciencerunner;
 
+enum BaseColumn {
+   DayofMonth(0),
+   DayOfWeek(1),
+   FlightDate(2),
+   UniqueCarrier(3),
+   TailNum(4),
+   OriginAirportID(5),
+   Origin(6),
+   OriginStateName(7),
+   DestAirportID(8),
+   Dest(9),
+   DestStateName(10),
+   DepTime(11),
+   DepDelay(12),
+   WheelsOff(13),
+   WheelsOn(14),
+   ArrTime(15),
+   ArrDelay(16),
+   Cancelled(17),
+   CancellationCode(18),
+   Diverted(19),
+   AirTime(20),
+   Distance(21)
+   ;
+   
+   private short id;
+   
+   BaseColumn(int i) {id = (short)i; }
+   
+   short getID(){ return id; }
+   
+}
+
+
+
+
+
 /**
  *
  * @author Kemper
